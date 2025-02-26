9da292106decbb696c72fbcd437781654e15cb10
yassinkazibwe <kazibweusama@gmail.com>
2025-02-26 18:58:23 +0300

    server create

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.get('/', (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-//   console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
